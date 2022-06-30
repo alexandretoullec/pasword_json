a10b7c91e5d7cefab5d74b6b753037877a5a2894
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2022-06-30 15:19:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+window = Tk()
+window.config(padx=50, pady=50)
+
+window.title("Password Manager")
+
+#functions
+
+def generate():
+    password_input.delete(0,END)
+    password = ""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    letters_list = [random.choice(letters) for _ in range(nr_letters)]
+    symbols_list = [random.choice(symbols) for _ in range(nr_symbols)]
+    number_list = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = letters_list + symbols_list + number_list
+    random.shuffle(password_list)
+
+
+    password = "".join(password_list)
+
+    # for char in password_list:
+    #     password += char
+
+    password_input.insert(0,password)
+    pyperclip.copy(password)
+
+
+def save():
+    website_save = website_input.get()
+    email_save = email_input.get()
+    password_save = password_input.get()
+    new_data = {
+        website_save:{
+            "email":email_save,
+            "password": password_save
+        }
+    }
+
+    if website_save == "" or password_save == "":
+        messagebox.showinfo(title="warning", message="You shouldn't let any field empty !!!")
+    else:
+        with open("data_files.json", "r") as data_file:
+            #load data
+            data = json.load(data_file)
+            #update data
+            data.update(new_data)
+
+        with open("data_files.json", "w") as data_file:
+            #write data
+            json.dump(data, data_file, indent=4)
+
+            website_input.delete(0,"end")
+            password_input.delete(0,"end")
+
+#Image
+canvas = Canvas(width=200, height=200)
+safe_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=safe_img)
+canvas.grid(row=0, column=1)
+
+
+#labels
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+
+#Entries
+
+website_input = Entry(width=52)
+website_input.focus()
+website_input.grid(column=1, row=1, columnspan=2)
+
+
+email_input = Entry(width=52)
+email_input.insert(0,"altoullec@yahoo.fr")
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_input = Entry(width=32)
+password_input.grid(column=1, row=3, sticky="W")
+
+
+#Buttons
+generate_button = Button(text="Generate", command=generate)
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=42, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky="W")
+
+
+
+
+window.mainloop()
